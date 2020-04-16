ebd4ac08e7dd2bb0b2cc390ff2536ce0b23bf48b
André Oliveira <inforoliver@gmail.com>
2020-04-15 22:05:51 -0300

    Click on the product to show details.. Ok

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,29 +10,37 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log('you clicked me')}>
-                        <Link to='/details'>
-                            <img src={img} alt="product" className="card-img-top" />
-                        </Link>
-                        <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {
-                            console.log('added to the cart')
-                        }}>
+                    <ProductConsumer>
+                        {value => (
+                            <div className=""
+                            onClick={() => value.handleDetail(id)}
+                                >
+                                <div className="img-container p-5" onClick={console.log('you clicked me')}>
+                                    <Link to='/details'>
+                                        <img src={img} alt="product" className="card-img-top" />
+                                    </Link>
+                                    <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {
+                                        value.addToCart(id);
+                                    }}>
 
-                            {
-                                inCart ? (<p className="text-capitalize mb-0" disabled>in Cart</p>)
-                                    : (<i className="fa fa-cart-plus"></i>)
-                            }
+                                        {
+                                            inCart ? (<p className="text-capitalize mb-0" disabled>in Cart</p>)
+                                                : (<i className="fa fa-cart-plus"></i>)
+                                        }
 
-                        </button>
-                    </div>
+                                    </button>
+                                </div>
 
-                    {/* card footer */}
-                    <div className="card-footer d-flex justify-content-between">
-                        <p className="alin-self-center mb-0">{title}</p>
-                        <h5 className="text-blue font-italic mb-0">
-                            <span className="mr-1">R$ {price}</span>
-                        </h5>
-                    </div>
+                                {/* card footer */}
+                                <div className="card-footer d-flex justify-content-between">
+                                    <p className="alin-self-center mb-0">{title}</p>
+                                    <h5 className="text-blue font-italic mb-0">
+                                        <span className="mr-1">R$ {price}</span>
+                                    </h5>
+                                </div>
+                            </div>
+                        )}
+                    </ProductConsumer>
                 </div>
             </ProductWrapper>
         );
@@ -42,11 +50,11 @@ export default class Product extends Component {
 // types of data mandatory
 Product.protoTypes = {
     product: PropTypes.shape({
-        id:PropTypes.number,
-        img:PropTypes.string,
-        title:PropTypes.string,
-        price:PropTypes.number,
-        inCart:PropTypes.bool
+        id: PropTypes.number,
+        img: PropTypes.string,
+        title: PropTypes.string,
+        price: PropTypes.number,
+        inCart: PropTypes.bool
     }).isRequired
 }
 
